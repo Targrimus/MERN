993b3ac6305363b706bc2d958a47847634bb4d08
RTK <tolga.kiran@outlook.com>
2024-09-15 13:15:38 +0300

    lesson 2 starts

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+/* Parse JSON Data */
+app.use(express.json());
 
-const PORT = process.env.PORT | 5000;
 
+/* Static files */
 app.use(express.static(path.join(__dirname,'public')));
 
+
+/* Main Router */
 const router = require('./routes/router');
 app.use('/', router );
 
 
+/* every other attempt to get */
 app.all('*', (req, res) =>{
     if(req.accepts('html')){
         res.sendFile(path.join(__dirname,'views','404.html'));
@@ -23,6 +28,11 @@ app.all('*', (req, res) =>{
     }
 });
 
+/* Port Name */
+const PORT = process.env.PORT | 5000;
+
+
+/* Server listen */
 const server = app.listen(PORT, (err) => {
     console.log("Server is running on port ", PORT);
 })
