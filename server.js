b67d9a283b5edbfa57a01e1d5d2dfbf9fa89fa73
RTK <tolga.kiran@outlook.com>
2024-09-15 12:31:30 +0300

    new files added

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+
+
 const PORT = process.env.PORT | 5000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const router = require('./routes/router');
+app.use('/', router );
+
+
+app.all('*', (req, res) =>{
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'));
+
+    }else if(req.accepts('json')){
+        res.json({message: '404 not found'});
+        
+    }else{
+        res.type('txt').send('404 not found');
+    }
+});
 
 const server = app.listen(PORT, (err) => {
     console.log("Server is running on port ", PORT);
